d27657a6bb393c0ca86a12a96c0336517351e461
Sergey Kovalevich <inndie@gmail.com>
2023-09-27 18:35:08 +0300

    update

--- app/__main__.py
+++ app/__main__.py
@@ -24,45 +24,51 @@ def get_commands():
     commands = []
     for name, command in config.commands.items():
         commands.append(BotCommand(name, command.description))
+    commands.append(BotCommand('list', 'list states'))
     commands.append(BotCommand('help', 'print help'))
     return commands
 
+def api_call(command, toggle=False):
+    router = config.routers[command.router]
+    api = connect(username=router.user, password=router.password, host=router.host)
+    match_expr = [ Key(key) == command.match[key] for key in command.match ]
+
+    # match record
+    data = api.path(command.path).select(Key('.id'), Key('disabled')).where(*match_expr)
+    data = tuple(data)[0]
+    # state
+    disabled = data['disabled']
+
+    if toggle:
+        # update
+        api.path(command.path).update(**{ 'disabled': not disabled, '.id': data['.id'] })
+
+    # disconnect
+    api.close()
+
+    return disabled
+
+def make_handler(name, command):
+    async def handler(message: Message):
+        notification_message = await message.answer('\N{SLEEPING SYMBOL}...')
+        try:
+            loop = asyncio.get_event_loop()
+            prevState = await loop.run_in_executor(None, api_call, command, True)
+            if prevState:
+                text = f'rule "{name}" state changed from OFF to ON'
+            else:
+                text = f'rule "{name}" state changed from ON to OFF'
+
+            await message.reply(text)
+        except Exception as ex:
+            await message.reply(f'\N{Heavy Ballot X} failed ({ex})')
+        finally:
+            await notification_message.delete()
+    return handler
+
 def register_commands(dp: Dispatcher):
     for name, command in config.commands.items():
-        router = config.routers[command.router]
-
-        async def handler(message: Message):
-            notification_message = await message.answer('\N{SLEEPING SYMBOL}...')
-            try:
-                # connect
-                api = connect(username=router.user, password=router.password, host=router.host, timeout=15)
-
-                match_expr = [ Key(key) == command.match[key] for key in command.match ]
-
-                # match record
-                data = api.path(command.path).select(Key('.id'), Key('disabled')).where(*match_expr)
-                data = tuple(data)[0]
-                # state
-                state = data['disabled']
-
-                # update
-                api.path(command.path).update(**{ 'disabled': not state, '.id': data['.id'] })
-
-                # disconnect
-                api.close()
-
-                if state:
-                    text = f'rule "{name}" state changed from OFF to ON'
-                else:
-                    text = f'rule "{name}" state changed from ON to OFF'
-
-                await message.reply(text)
-            except Exception as ex:
-                await message.reply(f'\N{Heavy Ballot X} failed ({ex})')
-            finally:
-                await notification_message.delete()
-
-        dp.register_message_handler(handler, commands=[name], chat_id=config.telegram.chat_id)
+        dp.register_message_handler(make_handler(name, command), commands=[name], chat_id=config.telegram.chat_id)
 
 async def startup(dp: Dispatcher):
     register_commands(dp)
@@ -71,6 +77,26 @@ async def startup(dp: Dispatcher):
 
 async def shutdown(dp: Dispatcher):
     pass
+
+@dp.message_handler(commands=['list'], chat_id=config.telegram.chat_id)
+async def command_list(message: Message):
+    notification_message = await message.answer('\N{SLEEPING SYMBOL}...')
+    try:
+        loop = asyncio.get_event_loop()
+
+        text = ''
+        for name, command in config.commands.items():
+            state = await loop.run_in_executor(None, api_call, command, False)
+            if state:
+                text += f'/{name} -> OFF\n'
+            else:
+                text += f'/{name} -> ON\n'
+
+        await message.reply(text)
+    except Exception as ex:
+        await message.reply(f'\N{Heavy Ballot X} failed ({ex})')
+    finally:
+        await notification_message.delete()
 
 @dp.message_handler(commands=['help'], chat_id=config.telegram.chat_id)
 async def command_help(message: Message):
